56ae72e7eb561539ae55214c10567ce81900cce4
Jay Deaton <jayrdeaton@gmail.com>
2026-08-27 15:16:08 +0700

    fleet update

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,9 +1,6 @@
 const { defineConfig } = require('eslint/config')
-const prettierRecommended = require('eslint-plugin-prettier/recommended')
-const simpleImportSort = require('eslint-plugin-simple-import-sort')
 const tsParser = require('@typescript-eslint/parser')
-const tsEslint = require('typescript-eslint')
-const packageJson = require('eslint-plugin-package-json')
+const base = require('@infinitetoken/eslint-config/npm-package')
 
 module.exports = defineConfig([
   {
@@ -15,31 +12,13 @@ module.exports = defineConfig([
       }
     }
   },
+  ...base,
   {
-    ignores: ['dist/**', 'node_modules/**', 'coverage/**', '**/*.js', '**/*.mjs', '**/*.cjs', 'src/__tests__/**']
-  },
-  ...tsEslint.configs.recommended,
-  prettierRecommended,
-  packageJson.configs.recommended,
-  {
-    extends: [packageJson.configs.recommended],
-    files: ['package.json'],
-    rules: {
-      'package-json/order-properties': 'warn',
-      'package-json/sort-collections': 'warn'
-    }
+    ignores: ['**/*.cjs', 'src/__tests__/**', 'tsup.config.ts']
   },
   {
-    plugins: {
-      'simple-import-sort': simpleImportSort
-    },
     rules: {
-      'prettier/prettier': 'warn',
-      'simple-import-sort/imports': 'warn',
-      'simple-import-sort/exports': 'warn',
-      'no-console': 'warn',
-      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-      '@typescript-eslint/no-require-imports': 'off'
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }]
     }
   }
 ])
